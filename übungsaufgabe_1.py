bbf0d1c6a66ab335cf2e1a7030de3ffbb1708c21
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2024-10-01 23:11:33 +0200

    Übungsaufgabe 1: Python-Grundlagen wird hinzugefügt

---
+++ übungsaufgabe_1.py
@@ -0,0 +1,6 @@
+first_name = "Rahimdad Faisal"
+last_name = "Safi"
+birth_year = 1991
+has_license = True
+
+print(f'My full name is {first_name} {last_name}. I was born in {birth_year}. Unfortunately, I have no license: {has_license}.')
