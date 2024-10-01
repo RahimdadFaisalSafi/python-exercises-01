ee767f5031a25d992306d2f1b6753a2b22bf96e6
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2024-10-01 23:32:54 +0200

    Übungsaufgabe 2: Python-Grundlagen wird hinzugefügt

---
+++ übungsaufgabe_2.py
@@ -0,0 +1,9 @@
+a = 10
+b = 3
+
+print("Summe von a und b:", a + b)
+print("Differenz von a und b:", a - b)
+print("Produkt von a und b:", a * b)
+print("Ganzzahliger Quotient von a und b:", a // b)
+print("Rest der Division von a und b:", a % b)
+print("Wert von a hoch b:", a ** b)
